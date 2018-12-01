dc3f11e447a0caf9897856e8b6469eed076b7f5c
Cody Helbling <cody.helbling@vacasa.com>
2018-12-01 08:04:43 -0700

    List of topics for future notes.

---
+++ Topics.js
@@ -0,0 +1,47 @@
+/**
+ IDE:
+ - Favorite plugins
+ - Key map
+ - Debugging Tools
+ - Build Tools
+ - Environment management tools
+
+ Programming Languages:
+ - JavaScript
+ - Python
+ - Clojure/ClojureScript
+ - SQL
+
+ Database
+ - Postgres
+ - MongoDB
+ - RDB Theory?
+
+ Cloud Computing
+ - AWS Lambda
+ - AWS RDS
+ - AWS S3
+ - AWS Route 53
+ - AWS CloudFront
+ - AWS EKS
+
+ Front-end Frameworks
+ - React
+ - Reagent
+
+ Back-end Frameworks
+ - Django
+ - Wagtail
+
+ DevOps
+ - Docker
+ - Kubernetes
+ - Terraform?
+
+ OS
+ - Linux
+
+ Algorithms?
+
+ Machine Learning?
+ */
