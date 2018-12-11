08aa81a6b3582bc7a7d0951d109ddfdde90f5556
Cody Helbling <cody.helbling@vacasa.com>
2018-12-11 08:55:23 -0700

    Started asycio Python lib notes.

---
+++ Python/asyncio.py
@@ -0,0 +1,15 @@
+"""
+What is asyncio?
+
+A Python library for writing single-threaded concurrent code using async/await syntax.
+https://docs.python.org/3/library/asyncio.html
+
+Often the perfect fit for IO-bound and high-level structured network code.
+
+API:
+- run Python coroutines concurrently and have full control over their execution
+- perform network IO and IPC
+- control subprocesses
+- distribute tasks via queues
+- synchronize concurrent code
+"""
