e8a6e9e40541ffde893ac72ecf7a57737467a675
Cody Helbling <cody.helbling@vacasa.com>
2019-05-01 05:34:20 -0600

    Ptyhon notes on finding the first occurrence in a list or string.

---
+++ Python/DataStructures/list.py
@@ -0,0 +1,2 @@
+# How can you find the first occurrence of a character in a list?
+[0, 1, 2].index(1)
---
+++ Python/string.py
@@ -0,0 +1,2 @@
+# How would you find the first occurrence of a character in a string?
+'avcd'.find('v')
